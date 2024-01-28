c87f10af8ba05da0432623ad008d942e58c5d5c5
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2024-01-28 21:27:13 +0530

    Tokenizes the input bibtex file.
    It does catch some errors however there is a bunch of errors like
    missing a ',' which it does not catch yet. The string searching is
    slow. Switch Case Used as a hashmap and does not gurantee input validity
    yet.
    
    A weird bug arises when the 'current' reaches the end of file. It
    generates an error. It does not however affect the file processing.

---
+++ tokenize.h
@@ -0,0 +1,50 @@
+#ifndef _TOKENIZE_H_
+#define _TOKENIZE_H_
+
+#include "b2j.h"
+
+typedef enum
+{
+	ARTICLE = 0,
+	BOOK,
+	BOOKLET,
+	CONFERENCE,
+	INBOOK,
+	INCOLLECTION,
+	INPROCEEDINGS,
+	MANUAL,
+	MASTERSTHESIS,
+	MISC,
+	PHDTHESIS,
+	PROCEEDINGS,
+	TECHREPORT,
+	UNPUBLISHED
+} BibType;
+
+typedef struct
+{
+	BibType type;	// type of bib
+	string_t tag;	// identifier of the bib
+
+	// Creating the types that we are interested in.
+	string_t title;
+	string_t author;	// this is a bit complicated, we will deal with this later.
+	int year;
+	int month;
+	int volume;
+	int number;
+	string_t pages;
+	string_t editor;
+	string_t series;
+	string_t organisation;
+	string_t journal;
+	string_t publisher;
+	string_t address;
+	string_t howPublished;
+	string_t bookTitle;
+	string_t notes;
+} bibtex_t;
+
+bibtex_t bibtex_tokenize(string_t source, size_t *current, size_t *line);
+
+#endif
