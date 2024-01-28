c3eee8510ebdeb411d7ad42bf5edc003aeaa1d13
NimComPoo-04 <bhattacharyyaaanjishnu.04@gmail.com>
2024-01-28 21:32:18 +0530

    Adds a string view untility structure

---
+++ b2j.h
@@ -0,0 +1,15 @@
+#ifndef _B2J_H_
+#define _B2J_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Utility structures and functions
+
+typedef struct
+{
+	char *value;
+	size_t length;
+} string_t;
+
+#endif
